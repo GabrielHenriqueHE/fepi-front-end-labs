e651924e41dde5b0fb84ca75fbd9532700b0ada9
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-06-03 01:13:01 -0300

    WIP: criando site para consulta de estatísticas do Github

---
+++ interface-api-github/main.js
@@ -0,0 +1,43 @@
+const searchField = document.querySelector("#search-field")
+const searchButton = document.querySelector("#search-button")
+
+const BASE_URL = "https://api.github.com/users"
+
+const fetchProfileData = async (username) => {
+    const data = await fetch(`${BASE_URL}/${username}`).then(response => response.json());
+
+    const userProfilePicture = document.querySelector("#user-profile-picture");
+    const userProfileUsername = document.querySelector("#user-username");
+    const userProfileName = document.querySelector("#user-profile-name");
+    const userProfileLocation = document.querySelector("#user-profile-location");
+    const userProfileFollowing = document.querySelector("#user-profile-following");
+    const userProfileFollowers = document.querySelector("#user-profile-followers");
+    const userProfileBio = document.querySelector("#user-profile-bio");
+
+    const dataRenderer = document.querySelector("#data");
+
+    dataRenderer.classList.remove("show");
+
+    setTimeout(() => {
+        userProfilePicture.setAttribute("src", data.avatar_url);
+        userProfileUsername.textContent = data.login;
+        userProfileName.textContent = data.name;
+        userProfileLocation.textContent = data.location;
+        userProfileFollowing.textContent = data.following;
+        userProfileFollowers.textContent = data.followers;
+        userProfileBio.textContent = data.bio != null ? data.bio : "Não informado";
+
+        dataRenderer.classList.add("show");
+    }, 300);
+};
+
+
+searchButton.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const username = searchField.value
+    
+    if (username.length > 0) {
+        fetchProfileData(username)
+    }
+})
